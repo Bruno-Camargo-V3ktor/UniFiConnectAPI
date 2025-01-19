99959611df600b07a4354b6a543189faa8f829ec
Bruno C. <v3ktoryt@gmail.com>
2025-01-18 21:37:50 -0300

    Feat: Adding a custom Serialization to convert a String to ObjectId, and vice versa

--- src/db/mongo_db.rs
+++ src/db/mongo_db.rs
@@ -1,5 +1,28 @@
 use rocket_db_pools::{Database, mongodb::Client};
 
+// Structs
 #[derive(Database)]
 #[database("mongodb")]
 pub struct MongoDb(Client);
+
+// Módulo para lidar com conversões de ObjectId <-> String
+pub mod serde_object_id {
+    use bson::oid::ObjectId;
+    use serde::{self, Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S>(id: &String, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let object_id = ObjectId::parse_str(id).map_err(serde::ser::Error::custom)?;
+        serializer.serialize_some(&object_id)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<String, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let object_id: ObjectId = Deserialize::deserialize(deserializer)?;
+        Ok(object_id.to_string())
+    }
+}
