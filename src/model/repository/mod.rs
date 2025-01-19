a46e8e7fa4fd0917daa145b9c6f63ee54467a2de
Bruno C. <v3ktoryt@gmail.com>
2025-01-18 21:42:00 -0300

    Feat: Creation of a generic repository behavior, to be used in implementing entity models that will be sent to the database

--- src/model/repository/mod.rs
+++ src/model/repository/mod.rs
@@ -1,1 +1,46 @@
+// Mods
+pub mod guest_repository;
 
+// Traits
+pub trait Repository {
+    type Id;
+    type Entity;
+    type Options;
+
+    async fn find_by_id(&self, id: Self::Id) -> Option<Self::Entity> {
+        todo!()
+    }
+    async fn find_one(&self, query: Self::Options) -> Option<Self::Entity> {
+        todo!()
+    }
+    async fn find(&self, query: Self::Options) -> Vec<Self::Entity> {
+        todo!()
+    }
+    async fn find_all(&self) -> Vec<Self::Entity> {
+        todo!()
+    }
+
+    async fn save(&self, entity: Self::Entity) -> Option<Self::Entity> {
+        todo!()
+    }
+    async fn save_all(&self, entitys: Vec<Self::Entity>) -> Vec<Self::Entity> {
+        todo!()
+    }
+
+    async fn update(&self, entity: Self::Entity) -> Option<Self::Entity> {
+        todo!()
+    }
+    async fn update_all(&self, query: Self::Options, modify: Self::Options) -> usize {
+        todo!()
+    }
+
+    async fn delete(&self, entity: Self::Entity) -> bool {
+        todo!()
+    }
+    async fn delete_by_id(&self, id: Self::Id) -> bool {
+        todo!()
+    }
+    async fn delete_all(&self, query: Self::Options) -> usize {
+        todo!()
+    }
+}
