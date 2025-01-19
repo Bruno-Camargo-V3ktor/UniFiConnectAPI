12b290faa970ff8b0048f0d40c2c19a73b4bd879
Bruno C. <v3ktoryt@gmail.com>
2025-01-19 20:43:09 -0300

    Refactor: Implementing a custom error creation method for failed request responses

--- src/utils/error.rs
+++ src/utils/error.rs
@@ -1,4 +1,7 @@
-use rocket::serde::{Deserialize, Serialize};
+use rocket::{
+    response::status::Custom,
+    serde::{Deserialize, Serialize, json::Json},
+};
 
 // Structs
 #[derive(Serialize, Deserialize)]
@@ -6,4 +9,17 @@ pub struct Error {
     pub err: String,
     pub time: String,
     pub status: u16,
+}
+
+// Impls
+impl Error {
+    pub fn new_with_custom(msg: &str, time: String, status: u16) -> Custom<Json<Self>> {
+        let error = Error {
+            err: msg.to_string(),
+            time,
+            status,
+        };
+
+        Custom(rocket::http::Status { code: status }, Json(error))
+    }
 }
