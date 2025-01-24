81b3cc347ee44a406c18fd24af50a95d98228a11
Bruno C. <v3ktoryt@gmail.com>
2025-01-24 19:25:18 -0300

    Feat: Creating a struct to centralize guest monitoring functions

--- src/utils/guest_utils.rs
+++ src/utils/guest_utils.rs
@@ -1,51 +1,95 @@
 use crate::{
-    model::entity::guest::{Guest, GuestStatus},
+    model::{
+        entity::guest::{Guest, GuestStatus},
+        repository::{Repository, guest_repository::GuestRepository},
+    },
     unifi::unifi::{ClientInfo, UnifiController},
 };
 
-// Functions
-#[allow(unused)]
-pub async fn check_and_update_clients_names(
-    unifi: &mut UnifiController,
-    guests: &Vec<Guest>,
-    clients: &Vec<ClientInfo>,
-) {
-    for g in guests {
-        let c = clients
-            .iter()
-            .find(|c| if g.mac == c.mac { true } else { false });
+use bson::doc;
+use rocket_db_pools::mongodb::Database;
 
-        if let Some(client) = c {
-            if client.name.is_none() {
-                unifi
-                    .rename_device_client(
-                        client.record_id.clone().unwrap(),
-                        g.site.clone(),
-                        format!("{} (Visitante)", g.full_name),
-                    )
-                    .await;
-            }
-        } else {
-            continue;
-        }
-    }
+// Struct
+pub struct GuestMonitoring {
+    sites: Vec<String>,
+    repo: GuestRepository,
+    unifi: UnifiController,
 }
 
-pub fn check_and_update_guest_status(guests: &mut Vec<Guest>, clients: &Vec<ClientInfo>) {
-    for g in guests {
-        if g.status != GuestStatus::Approved {
-            continue;
+// Impls
+impl GuestMonitoring {
+    pub fn new(sites: Vec<String>, database: Database, unifi: UnifiController) -> Self {
+        Self {
+            sites,
+            repo: GuestRepository {
+                database,
+                name: String::from("Guests"),
+            },
+            unifi,
         }
+    }
 
-        let c = clients
-            .iter()
-            .find(|c| if g.mac == c.mac { true } else { false });
+    pub async fn all(&mut self) {
+        let mut guests = self.repo.find_all().await;
 
+        let iter = self.sites.clone();
+        for site in iter {
+            let res = self.unifi.get_guest_clients(site.clone()).await;
+            if res.is_err() {
+                break;
+            }
+            let clients = res.unwrap();
 
-        if let Some(client) = c {
-            println!( "{client:?}" );
-            if client.expired.unwrap_or(true) {
-                g.status = GuestStatus::Expired;
+            self.check_and_update_guest_status(&mut guests, &clients);
+            self.check_and_update_clients_names(&guests, &clients).await;
+        }
+    }
+
+    pub async fn check_and_update_clients_names(
+        &mut self,
+        guests: &Vec<Guest>,
+        clients: &Vec<ClientInfo>,
+    ) {
+        for g in guests {
+            let c = clients
+                .iter()
+                .find(|c| if g.mac == c.mac { true } else { false });
+
+            if let Some(client) = c {
+                if client.name.is_none() {
+                    self.unifi
+                        .rename_device_client(
+                            client.record_id.clone().unwrap(),
+                            g.site.clone(),
+                            format!("{} (Visitante)", g.full_name),
+                        )
+                        .await;
+                }
+            } else {
+                continue;
+            }
+        }
+    }
+
+    pub fn check_and_update_guest_status(
+        &self,
+        guests: &mut Vec<Guest>,
+        clients: &Vec<ClientInfo>,
+    ) {
+        for g in guests {
+            if g.status != GuestStatus::Approved {
+                continue;
+            }
+
+            let c = clients
+                .iter()
+                .find(|c| if g.mac == c.mac { true } else { false });
+
+            if let Some(client) = c {
+                println!("{client:?}");
+                if client.expired.unwrap_or(true) {
+                    g.status = GuestStatus::Expired;
+                }
             }
         }
     }
