b190bdc52a26e13897786c2a2c6af9adb280c451
Bruno C. <v3ktoryt@gmail.com>
2025-01-25 18:39:44 -0300

    Refactor: Using custom methods and types from the 'Error' and 'Response' module in Admin Endpoints

--- src/controllers/admin_controller.rs
+++ src/controllers/admin_controller.rs
@@ -1,9 +1,6 @@
 use bcrypt::{DEFAULT_COST, hash, verify};
 use bson::doc;
-use chrono::Local;
 use rocket::fairing::Result;
-use rocket::http::Status;
-use rocket::response::status::{Accepted, Custom};
 use rocket::serde::json::Json;
 use rocket::{delete, post, put};
 
@@ -11,14 +8,15 @@ use crate::model::entity::admin::{Admin, AdminData, AdminLogin};
 use crate::model::repository::Repository;
 use crate::model::repository::admin_repositoy::AdminRepository;
 use crate::security::auth_jwt::create_token;
-use crate::utils::error::Error;
+use crate::utils::error::{BadRequest, CustomError, Error, Unauthorized};
+use crate::utils::responses::{Accepted, Created, Ok, Response};
 
 // ENDPOINTS
 #[post("/admin/login", data = "<data>")]
 pub async fn login(
     data: Json<AdminLogin>,
     repository: AdminRepository,
-) -> Result<Accepted<Json<String>>, Custom<Json<Error>>> {
+) -> Result<Accepted<String>, BadRequest> {
     let res = repository
         .find_one(doc! {
            "username" : &data.username
@@ -30,7 +28,7 @@ pub async fn login(
             let check = verify(&data.password, admin.password.unwrap().as_str());
             if let Ok(b) = check {
                 if b {
-                    return Ok(Accepted(Json(create_token(&admin.id))));
+                    return Ok(Response::new_accepted(create_token(&admin.id)));
                 }
             }
         }
@@ -38,11 +36,7 @@ pub async fn login(
         None => {}
     }
 
-    Err(Error::new_with_custom(
-        "Username or password not found",
-        Local::now().to_string(),
-        400,
-    ))
+    Err(Error::new_bad_request("Invalid Username or Password"))
 }
 
 #[post("/admin", data = "<data>")]
@@ -50,13 +44,9 @@ pub async fn create_admin(
     data: Json<AdminData>,
     repository: AdminRepository,
     admin: Option<Admin>,
-) -> Result<Custom<()>, Custom<Json<Error>>> {
+) -> Result<Created<()>, CustomError> {
     if admin.is_none() {
-        return Err(Error::new_with_custom(
-            "Unauthorized user",
-            Local::now().to_string(),
-            401,
-        ));
+        return Err(Error::new_unauthorized("Unauthorized user"));
     }
 
     let data = data.into_inner();
@@ -70,20 +60,13 @@ pub async fn create_admin(
     new_admin.password = match new_admin.password {
         Some(p) => Some(hash(p.as_str(), DEFAULT_COST).unwrap()),
         None => {
-            return Err(Custom(
-                Status::BadRequest,
-                Json(Error {
-                    time: Local::now().to_string(),
-                    err: String::from("field 'password' not found"),
-                    status: 404,
-                }),
-            ));
+            return Err(Error::new_bad_request("field 'password' not found"));
         }
     };
 
     let _ = repository.save(new_admin).await;
 
-    Ok(Custom(Status::Created, ()))
+    Ok(Response::new_created(()))
 }
 
 #[put("/admin", data = "<data>")]
@@ -91,13 +74,9 @@ pub async fn update_admin(
     data: Json<Admin>,
     repository: AdminRepository,
     admin: Option<Admin>,
-) -> Result<Custom<()>, Custom<Json<Error>>> {
+) -> Result<Ok<()>, CustomError> {
     if admin.is_none() {
-        return Err(Error::new_with_custom(
-            "Unauthorized user",
-            Local::now().to_string(),
-            401,
-        ));
+        return Err(Error::new_unauthorized("Unauthorized user"));
     }
 
     let mut admin = data.into_inner();
@@ -105,19 +84,12 @@ pub async fn update_admin(
     admin.password = match admin.password {
         Some(p) => Some(hash(p.as_str(), DEFAULT_COST).unwrap()),
         None => {
-            return Err(Custom(
-                Status::BadRequest,
-                Json(Error {
-                    time: Local::now().naive_local().to_string(),
-                    err: String::from("field 'password' not found"),
-                    status: 404,
-                }),
-            ));
+            return Err(Error::new_bad_request("field 'password' not found"));
         }
     };
 
     let _ = repository.update(admin).await;
-    Ok(Custom(Status::Ok, ()))
+    Ok(Response::new_ok(()))
 }
 
 #[delete("/admin/<id>")]
@@ -125,16 +97,12 @@ pub async fn delete_admin(
     id: String,
     repository: AdminRepository,
     admin: Option<Admin>,
-) -> Result<Custom<()>, Custom<Json<Error>>> {
+) -> Result<Ok<()>, Unauthorized> {
     if admin.is_none() {
-        return Err(Error::new_with_custom(
-            "Unauthorized user",
-            Local::now().to_string(),
-            401,
-        ));
+        return Err(Error::new_unauthorized("Unauthorized user"));
     }
 
     let _ = repository.delete_by_id(id).await;
 
-    Ok(Custom(Status::Ok, ()))
+    Ok(Response::new_ok(()))
 }
