1ae9270db35f7297322f8038eea3ff01533ae8e6
Bruno C. <v3ktoryt@gmail.com>
2025-01-27 20:58:11 -0300

    Feat: Checking if the approval code is within the validity date in the validation function

--- src/security/approval_code.rs
+++ src/security/approval_code.rs
@@ -1,11 +1,20 @@
 use crate::model::repository::{Repository, approver_repository::ApproverRepository};
 use bcrypt::verify;
+use chrono::Local;
 
 // Functions
 pub async fn validate_code(code: String, repository: &ApproverRepository) -> Option<String> {
     let approvers = repository.find_all().await;
+    let now = Local::now();
 
     for ap in approvers {
+        let validity_date = ap.validity.clone();
+        if let Some(date) = validity_date {
+            if now > date {
+                continue;
+            }
+        }
+
         let res = verify(&code, ap.secrete_code.as_str());
         match res {
             Ok(b) => {
