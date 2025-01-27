fe78374523f578212b014ef7f181e68360bd65e1
Bruno C. <v3ktoryt@gmail.com>
2025-01-26 22:49:09 -0300

    Feat: creating a method to validate a phone, validation is simple just based on the amount of characters

--- src/utils/validator.rs
+++ src/utils/validator.rs
@@ -2,4 +2,8 @@
 pub struct Validator;
 
 // Impls
-impl Validator {}
+impl Validator {
+    pub fn validate_phone(phone: String) -> bool {
+        if phone.len() == 11 { true } else { false }
+    }
+}
