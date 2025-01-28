9a00d4c894ae487a90ee5fdffb214a32e5cfb08f
Bruno C. <v3ktoryt@gmail.com>
2025-01-27 23:07:44 -0300

    Feat: Implemented Handle for status=400 errors in API requests

--- src/controllers/error_controller.rs
+++ src/controllers/error_controller.rs
@@ -1,4 +1,4 @@
-use crate::utils::error::{Error, NotFound};
+use crate::utils::error::{BadRequest, Error, NotFound};
 use rocket::{Catcher, Request, catch, catchers};
 
 #[catch(404)]
@@ -12,7 +12,12 @@ fn api_not_found(req: &Request) -> NotFound {
     )
 }
 
+#[catch(400)]
+fn api_bad_request(_req: &Request) -> BadRequest {
+    Error::new_bad_request("Invalid request body")
+}
+
 // Functions
 pub fn handles() -> Vec<Catcher> {
-    catchers![api_not_found]
+    catchers![api_bad_request, api_not_found]
 }
